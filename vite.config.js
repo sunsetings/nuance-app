418fb328398634f6731c6d924470de8319c9a629
sunsetings <sunsetcapital.ptyltd@gmail.com>
2026-03-31 20:55:10 +0900

    Build separate landing and app pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        app: resolve(__dirname, "app.html"),
+      },
+    },
+  },
+});
